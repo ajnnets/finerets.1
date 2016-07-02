c0f957b406a1d77c2c025bb065e2f9353f3cfe14
Prelang Builder <builder@prelang.com>
2016-07-02 16:52:41 +0000

    Generated scaffolding for model 'Area' via Rails

---
+++ app/views/areas/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @area, :id, :name, :region_id, :published, :created_at, :updated_at
---
+++ spec/views/areas/show.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "areas/show", type: :view do
+  before(:each) do
+    @area = assign(:area, Area.create!(
+      :name => "Name",
+      :region => nil,
+      :published => false
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(//)
+    expect(rendered).to match(/false/)
+  end
+end
